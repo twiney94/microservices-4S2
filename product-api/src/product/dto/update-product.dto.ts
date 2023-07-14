eaf9da04585d5093125033632e6706048426342d
Yassine Bekioui <mp482a@hotmail.fr>
2023-07-14 22:52:12 +0200

    add the 'update amount' method

--- product-api/src/product/dto/update-product.dto.ts
+++ product-api/src/product/dto/update-product.dto.ts
@@ -1,6 +1,5 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateProductDto } from './create-product.dto';
+import { PartialType } from "@nestjs/mapped-types";
+import { CreateProductDto } from "./create-product.dto";
 
 export class UpdateProductDto extends PartialType(CreateProductDto) {
-  id: number;
-}
+  
--- product-api/src/product/product.controller.ts
+++ product-api/src/product/product.controller.ts
@@ -1,35 +1,40 @@
-import { Controller } from '@nestjs/common';
-import { MessagePattern, Payload } from '@nestjs/microservices';
-import { ProductService } from './product.service';
-import { CreateProductDto } from './dto/create-product.dto';
-import { UpdateProductDto } from './dto/update-product.dto';
+import { Controller } from "@nestjs/common";
+import { MessagePattern, Payload } from "@nestjs/microservices";
+import { ProductService } from "./product.service";
+import { CreateProductDto } from "./dto/create-product.dto";
+import { UpdateProductDto } from "./dto/update-product.dto";
 
 @Controller()
 export class ProductController {
-  constructor(private readonly productService: ProductService) {}
+	constructor(private readonly productService: ProductService) {}
 
-  @MessagePattern('createProduct')
-  create(@Payload() createProductDto: CreateProductDto) {
-    return this.productService.create(createProductDto);
-  }
+	@MessagePattern("createProduct")
+	create(@Payload() createProductDto: CreateProductDto) {
+		return this.productService.create(createProductDto);
+	}
 
-  @MessagePattern('findAllProduct')
-  findAll() {
-    return this.productService.findAll();
-  }
+	@MessagePattern("findAllProduct")
+	findAll() {
+		return this.productService.findAll();
+	}
 
-  @MessagePattern('findOneProduct')
-  findOne(@Payload() id: number) {
-    return this.productService.findOne(id);
-  }
+	@MessagePattern("findOneProduct")
+	findOne(@Payload() id: number) {
+		return this.productService.findOne(id);
+	}
 
-  @MessagePattern('updateProduct')
-  update(@Payload() updateProductDto: UpdateProductDto) {
-    return this.productService.update(updateProductDto.id, updateProductDto);
-  }
+	@MessagePattern("updateProduct")
+	update(@Payload() updateProductDto: UpdateProductDto) {
+		return this.productService.update(updateProductDto.id, updateProductDto);
+	}
 
-  @MessagePattern('removeProduct')
-  remove(@Payload() id: number) {
-    return this.productService.remove(id);
-  }
+	@MessagePattern("removeProduct")
+	remove(@Payload() id: number) {
+		return this.productService.remove(id);
+	}
+
+	@MessagePattern("updateProductQuantity")
+	updateProductQuantity(@Payload() data: { id: number; amount: number }) {
+		return this.productService.updateProductQuantity(data.id, data.amount);
+	}
 }
